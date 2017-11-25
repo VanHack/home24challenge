c7f5cd943b0461556f3703c2673c3a06bb4289c7
ddmedeiros <ddmedeiros@icloud.com>
2017-11-25 21:35:48 -0200

    avatar at artist detail

--- src/components/artist/ArtistDetails.js
+++ src/components/artist/ArtistDetails.js
@@ -1,12 +1,42 @@
 import React from "react";
 import Grid from "material-ui/Grid";
+import Avatar from "material-ui/Avatar";
+import { withStyles } from "material-ui/styles";
 
-function ArtistDetails({ artist }) {
+const styles = {
+  row: {
+    display: "flex",
+    justifyContent: "center"
+  },
+  avatar: {
+    margin: 10,
+    width: 120,
+    height: 120
+  }
+};
+
+function ArtistDetails(props) {
+  const { artist, classes } = props;
   return (
     <Grid item xs={12}>
-      {artist.noArtistFound ? "Artist not found" : artist.name}
+      {artist.noArtistFound ? (
+        "Artist not found"
+      ) : (
+        <Grid container spacing={16} alignItems="center">
+          <Grid item xs={12} sm={2}>
+            <Avatar
+              alt={artist.name}
+              src={artist.thumb_url}
+              className={classes.avatar}
+            />
+          </Grid>
+          <Grid item xs={12} sm={10}>
+            {artist.name}
+          </Grid>
+        </Grid>
+      )}
     </Grid>
   );
 }
 
-export default ArtistDetails;
+export default withStyles(styles)(ArtistDetails);
